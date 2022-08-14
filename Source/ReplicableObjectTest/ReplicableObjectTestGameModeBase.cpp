8b997365a649b6f76eee8aa2bb494cac0e8bb5eb
Arseny Atman <->
2022-08-14 19:15:03 +0300

    fix: Fix the plugin structure

--- Source/ReplicableObjectTest/ReplicableObjectTestGameModeBase.cpp
+++ Source/ReplicableObjectTest/ReplicableObjectTestGameModeBase.cpp
@@ -3,3 +3,8 @@
 
 #include "ReplicableObjectTestGameModeBase.h"
 
+#include "AdvancedObject/AdvancedObject.h"
+#include "AdvancedObject/ReplicableObject.h"
+#include "AdvancedObject/ReplicatingActor.h"
+#include "AdvancedObject/ReplicatingActorComponent.h"
+#include "AdvancedObject/Replicator.h"
